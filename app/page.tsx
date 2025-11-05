62c341c32f32a0fef7313587970c28c232932c59
atefe fazeli <atefefazeli1996@outlook.com>
2025-11-05 16:18:49 +0330

    button component completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { ThemeToggle } from "@/components/themeToggle";
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-
   return (
     <div>
       <h1 className="text-rose-300 text-9xl">hello </h1>
-     <ThemeToggle />
+      <ThemeToggle />
+      <div className="flex gap-5 p-4">
+        <Button variant="primary">Primary Button</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="outline">Outline</Button>
+        <Button variant="ghost">Ghost</Button>
+        <Button variant="danger">Danger</Button>
+      </div>
     </div>
   );
 }
