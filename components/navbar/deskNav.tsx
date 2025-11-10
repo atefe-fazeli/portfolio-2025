facce4d40777e02a08b35e41b36633588e190860
atefe fazeli <atefefazeli1996@outlook.com>
2025-11-10 10:21:21 +0330

    change nav bg color

--- components/navbar/deskNav.tsx
+++ components/navbar/deskNav.tsx
@@ -11,7 +11,7 @@ const DesktopNav: React.FC<DesktopNavProps> = ({ navItems }) => {
 
 
  return (
-    <nav className="fixed top-0 left-0 right-0 z-50 h-14 bg-slate-900/80 backdrop-blur-lg border-b border-slate-700/50">
+    <nav className="fixed top-0 left-0 right-0 z-50 h-14 bg-[linear-gradient(90deg,#312640_0%,#312640_18%,#712281_52%,#712281_68%,#5a5788_100%)]">
       <div className="container mx-auto px-6 py-4">
         <div className="flex items-center justify-center gap-8">
          
@@ -19,7 +19,7 @@ const DesktopNav: React.FC<DesktopNavProps> = ({ navItems }) => {
               <Link
                 key={item.id}
                 href={item.href}
-                className="text-white hover:text-green-400 transition-colors duration-300 font-medium"
+                className="text-white hover:text-fuchsia-500 transition-colors duration-300 font-medium"
               >
                 {item.label}
               </Link>
