5c745696c9036a46c67fc5a9a267276631ee7295
atefe fazeli <atefefazeli1996@outlook.com>
2025-11-13 12:22:19 +0330

    create imagegallery component

---
+++ components/imageGallery.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import Image from 'next/image';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+interface ImageGalleryProps {
+  images: string[];
+}
+
+const ImageGallery = ({ images }: ImageGalleryProps) => {
+  return (
+    <div className="rounded-xl overflow-hidden shadow-lg">
+      <Swiper
+        modules={[Autoplay, Pagination, Navigation]}
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        navigation
+        loop={images.length > 1}
+        className="h-80 md:h-96"
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div className="relative w-full h-full">
+              <Image
+                src={image}
+                alt={`Project image ${index + 1}`}
+                fill
+                className="object-cover"
+                priority={index === 0}
+                sizes="(max-width: 768px) 100vw, 50vw"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ImageGallery;
